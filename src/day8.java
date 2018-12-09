258e859d82916dc2fe4d38b30f601b49229dcb84
Corey Lamb <corey.lamb@slalom.com>
2018-12-08 20:42:36 -0500

    works for test, but not for real data. :(

--- src/day8.java
+++ src/day8.java
@@ -1,15 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Stack;
 
 public class day8 {
     static String input;
     static int sum = 0;
-    static Stack<Integer> stack = new Stack<>();
     static String[] splitInput;
     static ArrayList<Integer> inputAsInt = new ArrayList<>();
-    static String[] listOfMetadata;
+    static int value = 0;
     public static void main(String[] args) {
         File file = new File("/Users/corey.lamb/Documents/workspace-sts-3.9.2.RELEASE/Advent2018/src/day8input");
         try {
@@ -24,9 +23,9 @@ public class day8 {
                 inputAsInt.add(Integer.valueOf(s));
             }
 
-            sum += getMetaData(inputAsInt);
-
+            sum += getMetaDataSum(inputAsInt);
             System.out.println(sum);
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -35,13 +34,26 @@ public class day8 {
         }
     }
 
-    private static int getMetaData(List<Integer> node){
+    private static int getMetaDataSum(List<Integer> node){
         int result = 0;
         int numOfChildren = node.get(0);
         int numOfMetadata = node.get(1);
 
+
+        // part 2
+        List<Integer> listOfChildrenToCount = new ArrayList<>();
+        HashMap<Integer, Integer> mapOfChildren = new HashMap<>();
+
+        for(int i : getListOfMetadata(node, numOfMetadata)){
+            if(i <= numOfChildren && i != 0){
+                listOfChildrenToCount.add(i);
+            }
+        }
+        //
+
         node.remove(1);
         node.remove(0);
+
         // termination case
         if (numOfChildren == 0){
             for (int i = 0; i < numOfMetadata; i++){
@@ -53,15 +65,54 @@ public class day8 {
         else {
 
             for (int i = 0; i < numOfChildren; i++){
-                result += getMetaData(node);
+
+                // part 1
+                //result += getMetaDataSum(node);
+                //
+
+                // part 2
+                mapOfChildren.put(i, getMetaDataSum(node));
             }
 
-            for (int i = 0; i < numOfMetadata; i++){
-                result += node.get(0);
-                node.remove(0);
+            for (int i : listOfChildrenToCount) {
+                if (mapOfChildren.containsKey(i-1)){
+                    result += mapOfChildren.get(i-1);
+                }
             }
+            //
+
+//            part 1
+//            for (int i = 0; i < numOfMetadata; i++){
+//                result += node.get(0);
+//                node.remove(0);
+//            }
         }
         return result;
+    }
+
+    //
+    // all below methods are only necesarry for part 2
+    //
+    private static List<Integer> getListOfMetadata(List<Integer> list, int numOfMetadata){
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < numOfMetadata; i++){
+            result.add(list.get(list.size()-(1+i)));
+        }
+        return result;
+    }
+
+    private static int countOccurancesAndRemove(List<Integer> list, int toCheck){
+        int count = 0;
+        for (int i : list){
+            if(i == toCheck){
+                count++;
+            }
+        }
+        for(int i = 0; i < count; i++){
+//            list.remove(list.indexOf(toCheck));
+        }
+        return count;
     }
 
 }
